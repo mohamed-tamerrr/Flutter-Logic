520d5cbd509dbc1f44b79837600571f5d6e4d19a
Mohamed Tamer <mohamed.tamerrr79@gmail.com>
2026-04-22 21:08:57 +0200

    Pagination

--- lib/root.dart
+++ lib/root.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_logic/screens/form_lesson.dart';
 import 'package:flutter_logic/screens/image_selection.dart.dart';
 import 'package:flutter_logic/screens/multi_selection.dart';
+import 'package:flutter_logic/screens/pagination.dart';
 import 'package:flutter_logic/screens/search_feature.dart';
 import 'package:flutter_logic/screens/single_selection.dart';
 import 'package:flutter_logic/screens/toggle_selection.dart';
@@ -30,6 +31,7 @@ class _RootState extends State<Root> {
     UploadFile(),
     SearchFeature(),
     FormLesson(),
+    Pagination(),
   ];
   int selectedIndex = 0;
   @override
